18ae8a526cf3cb1c3b59f3a8f4e3f3ffb8561394
Viktor Govako <viktor.govako@gmail.com>
2025-07-19 23:51:19 -0300

    Draw Bookmark's names.
    Signed-off-by: Viktor Govako <viktor.govako@gmail.com>

--- libs/map/framework.cpp
+++ libs/map/framework.cpp
@@ -2035,6 +2035,11 @@ place_page::Info & Framework::GetCurrentPlacePageInfo()
   return *m_currentPlacePageInfo;
 }
 
+void Framework::UpdateBookmarksTextPlacement()
+{
+  m_bmManager->UpdateBookmarksTextPlacement();
+}
+
 void Framework::ActivateMapSelection()
 {
   if (!m_currentPlacePageInfo)
@@ -2090,7 +2095,7 @@ void Framework::DeactivateMapSelection()
 
 void Framework::DeactivateMapSelectionCircle(bool restoreViewport)
 {
-  if (m_drapeEngine != nullptr)
+  if (m_drapeEngine)
     m_drapeEngine->DeselectObject(restoreViewport);
 }
 
