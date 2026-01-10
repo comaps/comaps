f9485d44f576523e8ab6fb423ace3c6c76233aa2
Leonardo Bishop <me@leonardobishop.net>
2026-01-10 13:55:20 +0000

    [drape] Remember navigation mode between sessions
    Signed-off-by: Leonardo Bishop <me@leonardobishop.net>

--- libs/map/framework.cpp
+++ libs/map/framework.cpp
@@ -206,6 +206,12 @@ void Framework::SwitchMyPositionNextMode()
     m_drapeEngine->SwitchMyPositionNextMode();
 }
 
+void Framework::StartPendingPositionMode()
+{
+  if (m_drapeEngine != nullptr)
+    m_drapeEngine->StartPendingPositionMode();
+}
+
 void Framework::SetMyPositionModeListener(TMyPositionModeChanged && fn)
 {
   m_myPositionListener = std::move(fn);
