b35574d228c6aa920333a8e277e284bb2f2d4fac
zyphlar <zyphlar@gmail.com>
2026-01-29 19:47:14 -0800

    Fix panoramax layer toggle crash and icon priority
    Signed-off-by: zyphlar <zyphlar@gmail.com>

--- libs/drape_frontend/frontend_renderer.cpp
+++ libs/drape_frontend/frontend_renderer.cpp
@@ -844,6 +844,7 @@ void FrontendRenderer::AcceptMessage(ref_ptr<Message> message)
   case Message::Type::SetSimplifiedTrafficColors:
   case Message::Type::UpdateMetalines:
   case Message::Type::EnableIsolines:
+  case Message::Type::EnablePanoramax:
   {
     m_forceUpdateScene = true;
     break;
