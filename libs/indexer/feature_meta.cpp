dd8447e394b7589f3abfcc018afae8db71b232d6
Jean-Baptiste <jeanbaptiste.charron@outlook.fr>
2026-02-08 13:56:58 +0100

    [core] Add support of population tag
    Signed-off-by: Jean-Baptiste <jeanbaptiste.charron@outlook.fr>

--- libs/indexer/feature_meta.cpp
+++ libs/indexer/feature_meta.cpp
@@ -198,6 +198,8 @@ bool Metadata::TypeFromString(string_view k, Metadata::EType & outType)
     outType = Metadata::FMD_CHARGE_SOCKETS;
   else if (k == "rooms")
     outType = Metadata::FMD_ROOMS;
+  else if (k == "population")
+    outType = Metadata::FMD_POPULATION;
   else
     return false;
 
@@ -326,6 +328,7 @@ string ToString(Metadata::EType type)
   case Metadata::FMD_NETWORK: return "network";
   case Metadata::FMD_CHARGE_SOCKETS: CHECK(false, ("FMD_CHARGE_SOCKETS is a compound attribute."));
   case Metadata::FMD_ROOMS: return "rooms";
+  case Metadata::FMD_POPULATION: return "population";
   case Metadata::FMD_COUNT: CHECK(false, ("FMD_COUNT can not be used as a type."));
   };
 
--- libs/indexer/feature_meta.hpp
+++ libs/indexer/feature_meta.hpp
@@ -126,6 +126,7 @@ public:
     FMD_BRANCH = 55,
     FMD_CHARGE_SOCKETS = 56,
     FMD_ROOMS = 57,
+    FMD_POPULATION = 58,
     FMD_COUNT
   };
 
