1c808bdd42e20e70aacec7554935883e709146f7
Jean-Baptiste <jeanbaptiste.charron@outlook.fr>
2026-02-14 16:03:05 +0100

    Fix core part
    Signed-off-by: Jean-Baptiste <jeanbaptiste.charron@outlook.fr>

--- generator/osm2meta.cpp
+++ generator/osm2meta.cpp
@@ -366,11 +366,6 @@ std::string MetadataTagProcessorImpl::ValidateAndFormat_charge(std::string v)
   return v;
 }
 
-std::string MetadataTagProcessorImpl::ValidateAndFormat_population(std::string v)
-{
-	return v;
-}
-
 std::string MetadataTagProcessorImpl::ValidateAndFormat_airport_iata(std::string const & v) const
 {
   if (!ftypes::IsAirportChecker::Instance()(m_params.m_types))
@@ -656,7 +651,7 @@ void MetadataTagProcessor::operator()(std::string const & k, std::string const &
   case Metadata::FMD_OUTDOOR_SEATING: valid = ValidateAndFormat_outdoor_seating(v); break;
   case Metadata::FMD_NETWORK: valid = ValidateAndFormat_operator(v); break;
   case Metadata::FMD_CHARGE_SOCKETS: m_chargeSockets.AggregateChargeSocketKey(k, v); break;
-  case Metadata::FMD_POPULATION: valid = ValidateAndFormat_population(v); break; 
+  // Population tag is already processed in an earlier stage
 
   // Metadata types we do not get from OSM.
   case Metadata::FMD_CUISINE:
