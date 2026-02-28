cc9947800c61549979b224a0e8f60d350392fbaf
Yannik Bloscheck <git@yannikbloscheck.com>
2026-02-28 15:36:47 +0100

    [ios] Added the code for verifying the signature
    Signed-off-by: Yannik Bloscheck <git@yannikbloscheck.com>

--- libs/platform/crypto/ed25519_apple.mm
+++ libs/platform/crypto/ed25519_apple.mm
@@ -1,11 +1,12 @@
 #include "platform/crypto/ed25519.hpp"
 #include "base/logging.hpp"
 
+#import "platform-Swift.h"
+
 namespace platform::crypto
 {
 bool VerifyEd25519(uint8_t const * pubKey, uint8_t const * msg, size_t msgSize, uint8_t const * sig)
 {
-  LOG(LWARNING, ("Ed25519 verification is not implemented on Apple platforms yet."));
-  return false;
+  return [Bridge verifyRegionsFileWithRawPublicKey:[NSData dataWithBytes:&pubKey length:sizeof(pubKey)] rawData:[NSData dataWithBytes:&msg length:sizeof(msg)] dataSize:msgSize rawSignature:[NSData dataWithBytes:&sig length:sizeof(sig)]];
 }
 } // namespace platform::crypto
