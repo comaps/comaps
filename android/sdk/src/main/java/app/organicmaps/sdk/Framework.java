674d7fbdcb8a19a2cbcae9bc773b9b589ca7063f
Yannik Bloscheck <git@yannikbloscheck.com>
2026-02-28 18:23:01 +0100

    [android] Support for modes logic
    Signed-off-by: Yannik Bloscheck <git@yannikbloscheck.com>

--- android/sdk/src/main/java/app/organicmaps/sdk/Framework.java
+++ android/sdk/src/main/java/app/organicmaps/sdk/Framework.java
@@ -297,6 +297,10 @@ public class Framework
 
   public static native boolean nativeIsOutdoorsLayerEnabled();
 
+  public static native void nativeSwitchToUsingVehicleStyle(boolean enabled);
+
+  public static native boolean nativeIsUsingVehicleStyle();
+
   @NonNull
   public static native MapObject nativeDeleteBookmarkFromMapObject();
 
