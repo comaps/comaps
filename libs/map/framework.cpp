35d94c08f6d8db4a7cbaa72b492751a406887862
Yannik Bloscheck <git@yannikbloscheck.com>
2026-03-11 23:40:28 +0100

    [core] More dynamic reloading
    Signed-off-by: Yannik Bloscheck <git@yannikbloscheck.com>

--- libs/map/framework.cpp
+++ libs/map/framework.cpp
@@ -1833,11 +1833,11 @@ void Framework::MarkMapStyle(MapStyle mapStyle)
   GetStyleReader().SetCurrentStyle(mapStyle);
 }
 
-void Framework::SetMapStyle(MapStyle mapStyle)
+void Framework::SetMapStyle(MapStyle mapStyle, bool const forceRerendering)
 {
   MarkMapStyle(mapStyle);
   if (m_drapeEngine != nullptr)
-    m_drapeEngine->UpdateMapStyle();
+    m_drapeEngine->UpdateMapStyle(forceRerendering);
   InvalidateUserMarks();
   UpdateMinBuildingsTapZoom();
 }
@@ -2617,7 +2617,7 @@ void Framework::SwitchToMapAppearance(MapAppearance const mapAppearance)
   else
     mapStyle = GetLightMapStyleVariant(mapStyle);
   
-  SetMapStyle(mapStyle);
+  SetMapStyle(mapStyle, true);
 }
 
 MapAppearance Framework::CurrentMapAppearance()
