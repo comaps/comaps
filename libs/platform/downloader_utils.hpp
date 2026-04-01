221010f8227938ed799d36b149284ee1b937ad66
Konstantin Pastbin <konstantin.pastbin@gmail.com>
2026-04-01 23:57:25 +0700

    [core] Download maps from MCAV folders
    Signed-off-by: Konstantin Pastbin <konstantin.pastbin@gmail.com>

--- libs/platform/downloader_utils.hpp
+++ libs/platform/downloader_utils.hpp
@@ -5,7 +5,10 @@
 
 namespace downloader
 {
+// Build an url to download a map file from.
 std::string GetFileDownloadUrl(std::string const & fileName, int64_t dataVersion, uint64_t diffVersion = 0);
+
+// Used in tests:
 bool IsUrlSupported(std::string const & url);
 std::string GetFilePathByUrl(std::string const & url);
 }  // namespace downloader
