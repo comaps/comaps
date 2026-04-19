82d74e209ffb948b8cd2a6b569f9a8ccfaf7e49e
Viktor Govako <viktor.govako@gmail.com>
2026-04-19 09:18:29 -0300

    [drape] New dispacing overlay tree for Bookmark titles.
    Signed-off-by: Viktor Govako <viktor.govako@gmail.com>

--- libs/drape_frontend/frontend_renderer.hpp
+++ libs/drape_frontend/frontend_renderer.hpp
@@ -224,6 +224,8 @@ private:
   void PrepareScene(ScreenBase const & modelView);
   void UpdateScene(ScreenBase const & modelView);
   void BuildOverlayTree(ScreenBase const & modelView);
+  void UpdateSearchMarkTextOverlay(ScreenBase const & modelView);
+  ref_ptr<dp::OverlayTree> GetOverlayTree(DepthLayer layerId) const;
 
   void EmitModelViewChanged(ScreenBase const & modelView) const;
 
@@ -349,6 +351,8 @@ private:
   drape_ptr<DrapeApiRenderer> m_drapeApiRenderer;
 
   drape_ptr<dp::OverlayTree> m_overlayTree;
+  // Actually, it holds displacing Bookmark titles. The tree named after SearchMarkLayer.
+  drape_ptr<dp::OverlayTree> m_searchMarkTextOverlayTree;
 
   FrameValues m_frameValues;
 
