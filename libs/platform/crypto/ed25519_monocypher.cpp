a1a5d3daec9f14c613b85527f35e2538aa0b4e01
Yannik Bloscheck <git@yannikbloscheck.com>
2026-04-23 14:58:34 +0200

    [ios] Make signature checking work
    Signed-off-by: Yannik Bloscheck <git@yannikbloscheck.com>

--- libs/platform/crypto/ed25519_monocypher.cpp
+++ libs/platform/crypto/ed25519_monocypher.cpp
@@ -4,7 +4,7 @@
 
 namespace platform::crypto
 {
-bool VerifyEd25519(uint8_t const * pubKey, uint8_t const * msg, size_t msgSize, uint8_t const * sig)
+bool VerifyEd25519(uint8_t const * pubKey, size_t pubKeySize, uint8_t const * msg, size_t msgSize, uint8_t const * sig, size_t sigSize)
 {
   return crypto_ed25519_check(sig, pubKey, msg, msgSize) == 0;
 }
