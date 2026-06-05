3bf4a714b595e83509d6d0a7853fdccce96be9c0
Yannik Bloscheck <git@yannikbloscheck.com>
2026-06-06 00:45:26 +0200

    [core] Limit special capacity tags to certain values
    Signed-off-by: Yannik Bloscheck <git@yannikbloscheck.com>

--- generator/osm2meta.cpp
+++ generator/osm2meta.cpp
@@ -399,8 +399,11 @@ std::string MetadataTagProcessorImpl::ValidateAndFormat_capacity(std::string v)
     return v;
 
   strings::NormalizeDigits(v);
-  //if (isdigit(v))
-  return v;
+  unsigned int i = 0;
+  if (strings::to_uint(v, i))
+    return v;
+  else
+    return {};
 }
 
 std::string MetadataTagProcessorImpl::ValidateAndFormat_local_ref(std::string const & v)
