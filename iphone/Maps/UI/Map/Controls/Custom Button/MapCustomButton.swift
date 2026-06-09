b85da8b57dfa629bd00a758f0fc0c52dc397e93f
Yannik Bloscheck <git@yannikbloscheck.com>
2026-06-09 21:46:58 +0200

    [ios] Glass map buttons for iOS 27
    Signed-off-by: Yannik Bloscheck <git@yannikbloscheck.com>

--- iphone/Maps/UI/Map/Controls/Custom Button/MapCustomButton.swift
+++ iphone/Maps/UI/Map/Controls/Custom Button/MapCustomButton.swift
@@ -34,7 +34,7 @@ struct MapCustomButton: View {
             } icon: {
                 kind.image
                     .symbolRenderingMode(hasBadge ? .palette : .monochrome)
-                    .foregroundStyle((hasBadge ? Color.BaseColors.red : .secondary), .secondary)
+                    .foregroundStyle((hasBadge ? Color.BaseColors.red : Color.mapButtonForeground), Color.mapButtonForeground)
                     .padding(.top, hasRecordBadge ? 3 : 0)
             }
         }
