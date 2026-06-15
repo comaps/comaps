80476d5420c7d0b080692f603dec0cf6e7daff0c
Yannik Bloscheck <git@yannikbloscheck.com>
2026-06-15 14:17:13 +0200

    [ios] Simplified foreground color logic of map buttons
    Signed-off-by: Yannik Bloscheck <git@yannikbloscheck.com>

--- iphone/Maps/UI/Map/Controls/Position Button/MapPositionButton.swift
+++ iphone/Maps/UI/Map/Controls/Position Button/MapPositionButton.swift
@@ -21,7 +21,7 @@ struct MapPositionButton: View {
                 Text(positionMode.description)
             } icon: {
                 positionMode.image
-                    .foregroundStyle(positionMode == .following || positionMode == .followingAndRotated ? Color.BaseColors.blue : Color.mapButtonForeground)
+                    .foregroundStyle(positionMode == .following || positionMode == .followingAndRotated ? Color.BaseColors.blue : Color.primary)
                     .padding(.top, positionMode == .locate || positionMode == .following ? 1 : 0)
             }
         }
