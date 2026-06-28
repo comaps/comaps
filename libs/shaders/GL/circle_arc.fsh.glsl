fedb64a9d24dd56884c5a929967d0f3762aaa4c8
Radek Olsak <radek@olsak.net>
2026-06-28 17:35:59 +0200

    [drape] [shaders] Improved line joins as circle arcs
    The circle arc shader is currently implemented
    using discard, as it is using big triangles
    that overlap, the transparency trick is not possible.
    
    Possible future addition would be to triangulate
    the circle arc more precisely using the inbetween
    of two regular n-gons. This would allow the shader
    not to use discard with the consequence of using
    more verticies. Usualy this tradeoff should be
    good on most graphics cards.
    
    Due to that still using full circles for lines
    that are not offset, as it should be faster.
    
    Note that because of the circle join refactor
    here, the antialiassing that remained on the
    circle cap is removed. This was a forgotten
    part that should have been removed in
    https://github.com/organicmaps/organicmaps/pull/6590
    
    Signed-off-by: Radek Olsak <radek@olsak.net>

--- libs/shaders/GL/circle_arc.fsh.glsl
+++ libs/shaders/GL/circle_arc.fsh.glsl
@@ -5,6 +5,9 @@ layout (location = 1) in vec2 v_colorTexCoords;
 layout (binding = 1) uniform sampler2D u_colorTex;
 #endif
 layout (location = 2) in vec2 v_uv;
+layout (location = 3) in float v_from;
+layout (location = 4) in float v_to;
+layout (location = 5) in float v_smallRadius;
 
 layout (location = 0) out vec4 v_FragColor;
 
@@ -27,8 +30,20 @@ void main()
 #else
   LOW_P vec4 finalColor = texture(u_colorTex, v_colorTexCoords);
 #endif
-  finalColor.a = finalColor.a * u_opacity;
-  if(length(v_uv) > 1.0)
-    finalColor.a = 0.0;
   v_FragColor = finalColor;
+  if(length(v_uv) > 1.0)
+    discard;
+  if(length(v_uv) < v_smallRadius)
+    discard;
+  float angle = atan(v_uv.y, v_uv.x);
+  if(v_from > v_to)
+    {
+      if(angle < v_from && angle > v_to)
+	discard;
+    }
+  else
+    {
+      if(angle < v_from || angle > v_to)
+	discard;
+    }
 }
--- libs/shaders/GL/circle_arc.vsh.glsl
+++ libs/shaders/GL/circle_arc.vsh.glsl
@@ -1,6 +1,9 @@
 layout (location = 0) in vec3 a_position;
 layout (location = 1) in vec2 a_pxOffset;
 layout (location = 2) in vec2 a_colorTexCoords;
+layout (location = 3) in float a_from;
+layout (location = 4) in float a_to;
+layout (location = 5) in float a_smallRadius;
 
 #ifdef ENABLE_VTF
 layout (location = 0) out LOW_P vec4 v_color;
@@ -8,6 +11,9 @@ layout (location = 0) out LOW_P vec4 v_color;
 layout (location = 1) out vec2 v_colorTexCoords;
 #endif
 layout (location = 2) out vec2 v_uv;
+layout (location = 3) out float v_from;
+layout (location = 4) out float v_to;
+layout (location = 5) out float v_smallRadius;
 
 layout (binding = 0) uniform UBO
 {
@@ -37,5 +43,8 @@ void main()
 #else
   v_colorTexCoords = a_colorTexCoords;
 #endif
+  v_from = a_from;
+  v_to = a_to;
+  v_smallRadius = a_smallRadius;
   v_uv = 2.0 * normalize(off); // `2 *` to have incircle with radius 1
 }
