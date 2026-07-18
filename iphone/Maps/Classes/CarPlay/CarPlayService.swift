aa7ab0b9f5b097f82856ce069c4c697c1dab4338
eisa01 <eisa01@gmail.com>
2026-07-18 09:24:32 +0200

    [carplay] Follow iOS logic for turn updates
    Generated with the help of GPT 5.6 Sol
    
    Signed-off-by: eisa01 <eisa01@gmail.com>

--- iphone/Maps/Classes/CarPlay/CarPlayService.swift
+++ iphone/Maps/Classes/CarPlay/CarPlayService.swift
@@ -1250,7 +1250,7 @@ extension CarPlayService: CarPlayRouterListener {
       let template = rootMapTemplate else {
         return
     }
-    router.updateEstimates()
+    router.updateEstimates(with: routeInfo)
     if let estimates = createEstimates(routeInfo: routeInfo) {
       template.updateEstimates(estimates, for: trip)
     }
--- iphone/Maps/Core/Framework/ProxyObjects/Routing/MWMRoutingManager.h
+++ iphone/Maps/Core/Framework/ProxyObjects/Routing/MWMRoutingManager.h
@@ -10,7 +10,7 @@ NS_SWIFT_NAME(RoutingManagerListener)
 @protocol MWMRoutingManagerListener <NSObject>
 - (void)processRouteBuilderEventWithCode:(MWMRouterResultCode)code
                                countries:(NSArray<NSString *> *)absentCountries;
-- (void)didLocationUpdate:(NSArray<NSString *> *)notifications;
+- (void)didLocationUpdate:(NSArray<NSString *> *)notifications routeInfo:(nullable RouteInfo *)routeInfo;
 - (void)updateCameraInfo:(BOOL)isCameraOnRoute speedLimitMps:(double)limit NS_SWIFT_NAME(updateCameraInfo(isCameraOnRoute:speedLimitMps:));
 @end
 
