f43a0a0e56d1d55151a17f8676312b11d988ddf0
Viktor Govako <viktor.govako@gmail.com>
2026-07-31 19:34:17 +0200

    [map] Draw mwm boundaries.
    Signed-off-by: Viktor Govako <viktor.govako@gmail.com>
    
    # Conflicts:
    #	libs/map/CMakeLists.txt
    #	libs/storage/CMakeLists.txt

--- libs/storage/country_info_getter.hpp
+++ libs/storage/country_info_getter.hpp
@@ -20,7 +20,9 @@
 #include <type_traits>
 #include <vector>
 
+
 #include "3party/ankerl/unordered_dense.h"
+class FeatureType;
 
 class Platform;
 
@@ -52,6 +54,14 @@ public:
   std::vector<CountryDef> const & GetCountries() const { return m_countries; }
 
   RegionId GetRegionId(CountryId const & countryId) const;
+
+  template <class FnT>
+  void ForEachRegionId(std::vector<CountryId> const & countries, FnT && fn) const
+  {
+    for (RegionId id = 0; id < m_countries.size(); ++id)
+      if (std::binary_search(countries.begin(), countries.end(), m_countries[id].m_countryId))
+        fn(id);
+  }
 
 protected:
   // Returns identifier of the first country containing |pt| or |kInvalidId| if there is none.
@@ -147,6 +157,8 @@ public:
   // Loads all regions for country number |id| from |m_reader|.
   std::vector<m2::RegionD> LoadRegionsFromDisk(RegionId id) const;
 
+  void GetTriangles(RegionId id, FeatureType & ft) const;
+
 protected:
   CountryInfoReader(ModelReaderPtr polyR, ModelReaderPtr countryR);
 
@@ -160,8 +172,10 @@ protected:
   auto WithRegion(RegionId id, Fn && fn) const;
 
   FilesContainerR m_reader;
-  mutable base::Cache<uint32_t, std::vector<m2::RegionD>> m_cache;
-  mutable std::mutex m_cacheMutex;
+
+  mutable base::Cache<uint32_t, std::vector<m2::RegionD>> m_polyCache;
+  mutable base::Cache<uint32_t, std::vector<m2::PointD>> m_trgCache;
+  mutable std::mutex m_polyMutex, m_trgMutex;
 };
 
 // This class allows users to get info about very simply rectangular
