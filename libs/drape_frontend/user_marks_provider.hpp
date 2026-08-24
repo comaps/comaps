organicmaps/organicmaps

Draw Bookmark's names.
Signed-off-by: Viktor Govako <viktor.govako@gmail.com>

libs/drape_frontend/user_marks_provider.hpp
@@ -12,7 +12,7 @@
 
 #include "indexer/feature_decl.hpp"
 
-#include "geometry/polyline2d.hpp"
+#include "platform/placement_sett.hpp"
 
 #include <vector>
 
@@ -58,14 +58,20 @@ public:
   kml::MarkId GetId() const { return m_id; }
   virtual kml::MarkGroupId GetGroupId() const = 0;
 
+  /// @todo Add/modify current functions if you have some new requirements ..
+  /// Probably, we can fix it by adding some type-based UM (Providers) processing.
   virtual m2::PointD const & GetPivot() const = 0;
   virtual m2::PointD GetPixelOffset() const = 0;
   virtual dp::Anchor GetAnchor() const = 0;
   virtual bool GetDepthTestEnabled() const = 0;
   virtual float GetDepth() const = 0;
-  virtual DepthLayer GetDepthLayer() const = 0;
   virtual bool IsVisible() const = 0;
+
+  virtual DepthLayer GetDepthLayer() const = 0;
   virtual drape_ptr<TitlesInfo> GetTitleDecl() const = 0;
+  virtual DepthLayer GetDepthLayer(settings::Placement) const { return GetDepthLayer(); }
+  virtual drape_ptr<TitlesInfo> GetTitleDecl(settings::Placement) const { return GetTitleDecl(); }
+
   virtual drape_ptr<SymbolNameZoomInfo> GetSymbolNames() const = 0;
   virtual drape_ptr<ColoredSymbolZoomInfo> GetColoredSymbols() const = 0;
   virtual drape_ptr<SymbolSizes> GetSymbolSizes() const = 0;

comaps/comaps

libs/drape_frontend/user_marks_provider.hpp
@@ -13,6 +13,8 @@
 
 #include "indexer/feature_decl.hpp"
 
+#include "platform/placement_settings.hpp"
+
 #include "geometry/point2d.hpp"
 
 #include <vector>
@@ -59,14 +61,20 @@ public:
   kml::MarkId GetId() const { return m_id; }
   virtual kml::MarkGroupId GetGroupId() const = 0;
 
+  /// @todo Add/modify current functions if you have some new requirements ..
+  /// Probably, we can fix it by adding some type-based UM (Providers) processing.
   virtual m2::PointD const & GetPivot() const = 0;
   virtual m2::PointD GetPixelOffset() const = 0;
   virtual dp::Anchor GetAnchor() const = 0;
   virtual bool GetDepthTestEnabled() const = 0;
   virtual float GetDepth() const = 0;
-  virtual DepthLayer GetDepthLayer() const = 0;
   virtual bool IsVisible() const = 0;
+
+  virtual DepthLayer GetDepthLayer() const = 0;
   virtual drape_ptr<TitlesInfo> GetTitleDecl() const = 0;
+  virtual DepthLayer GetDepthLayerEx(settings::Placement) const { return GetDepthLayer(); }
+  virtual drape_ptr<TitlesInfo> GetTitleDeclEx(settings::Placement) const { return GetTitleDecl(); }
+
   virtual drape_ptr<SymbolNameZoomInfo> GetSymbolNames() const = 0;
   virtual drape_ptr<ColoredSymbolZoomInfo> GetColoredSymbols() const = 0;
   virtual drape_ptr<SymbolSizes> GetSymbolSizes() const = 0;