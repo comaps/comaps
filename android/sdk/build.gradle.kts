f81ecf078034a6274dc4bec46610e8f8ea24cddb
Penn Mackintosh <git@pennmack.co.uk>
2026-09-21 23:13:47 +0100

    [android] fix intermittent linker failures
    CMake prior to 3.25.3 ignores the NDK version when determining the
    correct linker to use, and incorrectly uses gold.
    That causes an error due to the unknown -fuse-ld=gold flag.
    
    See https://github.com/android/ndk/issues/1444#issuecomment-2977723268
    
    Signed-off-by: Penn Mackintosh <git@pennmack.co.uk>

--- android/sdk/build.gradle.kts
+++ android/sdk/build.gradle.kts
@@ -83,7 +83,7 @@ android {
 
     externalNativeBuild {
         cmake {
-            version = "3.22.1+"
+            version = "3.25.3+"
             path = File("../../CMakeLists.txt")
         }
     }
